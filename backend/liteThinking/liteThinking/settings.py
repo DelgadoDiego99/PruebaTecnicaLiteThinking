3f314d7eb5945484e70b4716d4e4b1710d102fad
DelgadoDiego99 <delgadodie99@gmail.com>
2022-07-26 07:46:57 -0500

    Changes to settings (Cors)

--- backend/liteThinking/liteThinking/settings.py
+++ backend/liteThinking/liteThinking/settings.py
@@ -139,23 +139,3 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
-
-CORS_ALLOWED_ORIGINS = [
-    "*",
-]
-
-CORS_ALLOW_METHODS = [
-    "DELETE",
-    "GET",
-    "POST",
-    "PUT",
-]
-
-CORS_ALLOW_HEADERS = (
-        'x-requested-with',
-        'content-type',
-        'accept',
-        'origin',
-        'authorization',
-        'x-csrftoken'
-)
